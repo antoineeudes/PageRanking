82902f30ef221307bbc3684a798a2b4430902eac
Unknown <alexperez@wanadoo.fr>
2019-05-06 10:45:41 +0200

    Added pi_iterative

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import numpy as np
+from numpy.linalg import norm
+
+eps = 0.001
 
 def create_Adj(w, h):
     return np.one((w, h)) - np.eye((w, h))
@@ -12,6 +15,18 @@ def google(Adj):
     z = 0
     alpha = 0
     return P, Pss, Pprim, d, z, alpha
+
+def pi_iterative(P_prime):
+    m, n = P_prime.shape
+    pn = np.ones(n)/n
+
+    while True:
+        p, pn = pn, np.dot(P_prime, p)
+
+        if norm(pn-p, np.inf) < eps:
+            break
+
+    return p
 
 if __name__=='__main__':
     Adj = create_Adj((3,3))
