215e3f9628cab2f1f009841e7f9979b15e3a7e53
Antoine <antoine.eudes@gmail.com>
2019-05-06 11:13:08 +0200

    added P,Prim,Pss function

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
 import numpy as np
 
-def create_Adj(w, h):
-    return np.one((w, h)) - np.eye((w, h))
+def create_Adj(w):
+    return np.ones((w,w)) - np.identity(w)
 
 def google(Adj):
-    w, h = Adj.shape()
-    P = np.zeros((w, h))
-    Pss = np.zeros((w, h))
-    Pprim = np.zeros((w, h))
-    d = 0
-    z = 0
-    alpha = 0
+    w, h = Adj.shape
+
+    # Choix de paramètres
+    z = np.ones((1, h))/w
+    alpha = 0.1
+    e = np.ones((w,1))
+
+    # Calcul de d
+    sum_cols = Adj.sum(axis=1)
+    d = (sum_cols==0).reshape((w,1))
+
+    # Calcul de Pss
+    degs = Adj.sum(axis=1)
+    Pss = (Adj/degs)
+    P = alpha*Pss + np.dot(d,z) + (1-alpha)*np.dot((e-d), z)
+    Pprim = P.T
     return P, Pss, Pprim, d, z, alpha
 
 if __name__=='__main__':
-    Adj = create_Adj((3,3))
+    Adj = create_Adj(3)
     print(google(Adj))
